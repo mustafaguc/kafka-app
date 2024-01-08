1849b70b4e18242718446b970812de19dd511de5
mustafaguc <mustafaguch@gmail.com>
2024-01-08 03:40:51 +0300

    removed explicit admin configs

--- src/main/java/com/example/kafkaapp/LinkProducerApplication.java
+++ src/main/java/com/example/kafkaapp/LinkProducerApplication.java
@@ -14,17 +14,6 @@ import java.util.Map;
 @SpringBootApplication
 public class LinkProducerApplication {
 
-    @Bean
-    public KafkaAdmin admin(@Value("${spring.kafka.bootstrap-servers}") String kafkaBrokers,
-                            @Value("${spring.kafka.properties.sasl.jaas.config}") String jaasConfig) {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBrokers);
-        configs.put(AdminClientConfig.DEFAULT_SECURITY_PROTOCOL, "PLAINTEXT");
-        configs.put(SaslConfigs.SASL_MECHANISM, "GSSAPI");
-        configs.put(SaslConfigs.SASL_JAAS_CONFIG, jaasConfig);
-        return new KafkaAdmin(configs);
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(LinkProducerApplication.class, args);
     }
