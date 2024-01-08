a85f7fa26c7d076846a3c12cc93a88cb349d8754
mustafaguc <mustafaguch@gmail.com>
2024-01-08 03:30:54 +0300

    updated admin settings

--- src/main/java/com/example/kafkaapp/LinkProducerApplication.java
+++ src/main/java/com/example/kafkaapp/LinkProducerApplication.java
@@ -1,6 +1,7 @@
 package com.example.kafkaapp;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.common.config.SaslConfigs;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,9 +15,13 @@ import java.util.Map;
 public class LinkProducerApplication {
 
     @Bean
-    public KafkaAdmin admin(@Value("${spring.kafka.bootstrap-servers}") String kafkaBrokers) {
+    public KafkaAdmin admin(@Value("${spring.kafka.bootstrap-servers}") String kafkaBrokers,
+                            @Value("${spring.kafka.properties.sasl.jaas.config}") String jaasConfig) {
         Map<String, Object> configs = new HashMap<>();
         configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBrokers);
+        configs.put(AdminClientConfig.DEFAULT_SECURITY_PROTOCOL, "PLAINTEXT");
+        configs.put(SaslConfigs.SASL_MECHANISM, "GSSAPI");
+        configs.put(SaslConfigs.SASL_JAAS_CONFIG, jaasConfig);
         return new KafkaAdmin(configs);
     }
 
